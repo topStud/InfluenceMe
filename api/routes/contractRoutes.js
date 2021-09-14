bd7ba5fff5c7b1047df15ebb7476e6aaa822f0e1
batelpirov1@gmail.com <batelpirov1@gmail.com>
2021-09-14 21:50:33 +0300

    fixed

--- api/routes/contractRoutes.js
+++ api/routes/contractRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const contractController = require('../controllers/contractController')
 
 router.post('/', contractController.addContract)
-router.get('/:id', contractController.contractsOf)// id of user
+router.get('/of/:id', contractController.contractsOf)// id of user
 router.get('/:id', contractController.findContract)// id of contract
 
 
